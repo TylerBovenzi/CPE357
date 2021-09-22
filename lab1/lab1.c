001bd2fbb0be523f16a7526dd77d045c93ca39a5
TylerBovenzi <tylerbovenzi4427@gmail.com>
2021-09-22 06:55:03 +0000

    Added comments

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 void lineout(int leaves, int cols){
-        for(int j=0; j<cols;j++){
-            if(abs(j-(cols-1)/2)<=leaves)
-                printf("*");
+        for(int j=0; j<cols;j++){   //foreach character in a row
+            if(abs(j-(cols-1)/2)<=leaves)   //calc if in range
+                printf("*");                
             else
                 printf(" ");
         }
-        printf("\n");
+        printf("\n");           //newline
 }
 
 void main(){
     int layers, cols;
-    layers=0; cols=21;
+    layers=0; cols=21;          //Mathmatical Defs
     printf("Number of leave lines: ");
-    scanf("%d", &layers);
-    if(layers>10) layers = 10;
-    for(int i=0; i<layers;i++)
+    scanf("%d", &layers);       //collects #of lines
+    if(layers>10) layers = 10;  //truncates to 10
+    for(int i=0; i<layers;i++)  //prints leaves
         lineout(i,cols);
-    for(int i=0; i<3;i++)
+    for(int i=0; i<3;i++)       //prints stump
         lineout(0, cols);
 }
