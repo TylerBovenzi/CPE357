cfe69fdff2550a5e258a2cd54d038a4a53a345a3
TylerBovenzi <tylerbovenzi4427@gmail.com>
2021-10-12 05:56:01 +0000

    add assignment 2

---
+++ assignment2/assignment2.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#define PAGESIZE 4096
+
+//unsigned char myheap[1048576];
+
+struct chunkhead{
+    unsigned int size;
+    unsigned int info;
+    struct chunkhead *next,*prev;
+}chunkhead;
+struct chunkhead *head;
+
+char* mymalloc(int size){
+    if(size<=0) return 0;   //handle bad inputs
+    struct chunkhead *addr = head;
+    size+=24; //add 24 to size to handle chunk
+    int pages = 1+(int)(( (float)size / (float) PAGESIZE));
+    if(size % PAGESIZE) size = PAGESIZE*pages;      //only modify if not clean
+    if(!head){
+        head = sbrk(size);
+        head->size=size;     //sizeof(chunkhead);
+        head->info=1;           //empty head;
+        head->next=0;head->prev=0;
+        return (char *)(head +1);
+    }
+    while(addr){                                    //iterate through all chunks
+        if(addr->size >= size && !addr->info){      //check for suitable chunk
+            //suitable chunk is found
+            int sizeDif = addr->size - size;        
+            addr->size=size;                        //set new size of addr  
+            addr->info=1;                           //set chunk as occupied 
+            if(sizeDif){                            //handle splitting
+                int ima = ((char*)addr)+size-((char*)head);
+                
+                //struct chunkhead *newNext = (struct chunkhead*)&myheap[ima];
+                struct chunkhead *newNext = (struct chunkhead*)((char*)(addr)+size);
+                newNext->size = sizeDif;
+                newNext->info = 0;
+                newNext->prev = addr;
+                if(addr->next){
+                    newNext->next = addr->next;
+                    newNext->next->prev = newNext;
+                }
+                addr->next = newNext;               //link to next
+            }
+            return (char*)(addr+1);
+        } else {                                    //no suitable chunk
+            addr = addr->next;                      //check next
+        }
+    }
+    struct chunkhead* new = 
+
+    return 0;                                       //no available chunks
+}
+
+void myfree(char *address){
+    struct chunkhead *addr = (struct chunkhead*)address;
+    addr-=1;
+    if(addr == head && !head->next){
+        head = 0;
+        sbrk(addr->size);
+        return;
+    }
+
+    addr->info = 0;
+    if(addr->prev && !addr->prev->info){  //if there is an empty previous
+        addr->prev->size = addr->size+addr->prev->size;
+        addr->prev->next = addr->next;
+        if(addr->next) addr->next->prev = addr->prev;
+        addr = addr->prev;
+    }
+    if(addr->next && !addr->next->info){  //if there is an empty next
+        addr->size = addr->size+addr->next->size;
+        addr->next = addr->next->next;
+        if(addr->next) addr->next->prev = addr;
+    }
+    if(!addr->next){
+        addr->prev->next = 0;
+        sbrk(addr->size);
+        return;
+    }
+}
+
+void analyse(){
+    if(!head) return;
+    struct chunkhead *addr = head;
+    int i=1;
+    while(addr){
+        printf("Chunk #%d:\n", i);
+        printf("\tSize: %d\n", addr->size);
+        if(addr->info) printf("\t\033[0;31mOccupied\033[0m\n");
+        else printf ("\t\033[0;32mFree\033[0m\n");
+        printf("\tPrev: %p\n", addr->prev);
+        printf("\tNext: %p\n", addr->next);
+        i++;
+        addr=addr->next;
+    }
+}
+
+void main(){
+    char *a = mymalloc(12000);
+    char *b = mymalloc(5000);
+    //char *c = mymalloc(5000);
+    
+    //myfree(a);
+    //char *c = mymalloc(2000);
+    //b = mymalloc(1000);
+
+    analyse();
+
+
+}
